f971eb8184ad57093fdb9f12e52cbc41722d1642
kumar7123 <sarsun234@234@gmail.com>
2022-10-08 14:37:02 +0530

    Second

--- src/org/company/Company.java
+++ src/org/company/Company.java
@@ -4,4 +4,9 @@ public class Company {
 public void companyName() {
 	System.out.println("kr technolwgies");
 }
+private void emp() {
+	System.out.println("Male=54");
+	System.out.println("Female=67");
+
+}
 }
